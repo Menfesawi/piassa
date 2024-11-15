1e62712c76b75604bd3cd895c04f22ce7af76db6
Menfesawi <menfesawisimon123@gmail.com>
2024-11-15 09:35:32 +0300

    My message

--- wp-content/themes/piassa/functions.php
+++ wp-content/themes/piassa/functions.php
@@ -4,4 +4,11 @@
  * Theme Functions
  * @package Piassa
  * 
+ * 
  */
+
+ function piassa_scripts(){
+    wp_enqueue_style('stylesheet', get_stylesheet_uri(), [], filemtime(get_stylesheet_directory()) );
+ }
+
+ add_action('wp_enqueue_scripts','piassa_scripts');
--- wp-content/themes/piassa/index.php
+++ wp-content/themes/piassa/index.php
@@ -7,8 +7,8 @@
 get_header();
 ?>
 <div class="content">
-    <h2>Website Content</h2>
+    <h2><?php  echo filemtime(get_template_directory().'/style.css'); ?></h2>
 </div>
 
 <?php 
-get_footer( );
+get_footer();
