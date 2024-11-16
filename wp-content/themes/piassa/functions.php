a06ce9ed169dfa110d4a17641ac05ff0c158319a
Menfesawi <menfesawisimon123@gmail.com>
2024-11-16 23:15:50 +0300

    Hello Time

--- wp-content/themes/piassa/functions.php
+++ wp-content/themes/piassa/functions.php
@@ -8,7 +8,7 @@
  */
 
  function piassa_scripts(){
-    wp_enqueue_style('stylesheet', get_stylesheet_uri(), [], filemtime(get_stylesheet_directory()) );
+    wp_enqueue_style('stylesheet', get_stylesheet_uri(), [], filemtime(get_stylesheet_directory()), 'all' );
  }
 
  add_action('wp_enqueue_scripts','piassa_scripts');
