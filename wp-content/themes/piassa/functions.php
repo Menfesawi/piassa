3cd349dedb2a1f00a60fc714ca13171ae5e72d32
Menfesawi <menfesawisimon123@gmail.com>
2024-11-20 17:47:32 +0300

    After Video

--- wp-content/themes/piassa/functions.php
+++ wp-content/themes/piassa/functions.php
@@ -8,7 +8,6 @@
  */
 
  function piassa_scripts(){
-    wp_enqueue_style('stylesheet', get_stylesheet_uri(), [], filemtime(get_stylesheet_directory()), 'all' );
+   wp_enqueue_style('stylesheet', get_stylesheet_uri(), [], filemtime(get_template_directory().'/style.css'), 'all');
  }
-
  add_action('wp_enqueue_scripts','piassa_scripts');
--- wp-content/themes/piassa/index.php
+++ wp-content/themes/piassa/index.php
@@ -7,8 +7,7 @@
 get_header();
 ?>
 <div class="content">
-    <h2><?php  echo 'The last version of CSS modification is:<br>'. filemtime(get_stylesheet_directory()) ?></h2>
-    <h2 style="color:blueviolet">filemtime<span style="color: orange;">(<span style="color: blueviolet;">get_stylesheet_directory</span><span style="color: green;">()</span>)</span></h2>
+    <h2><?php echo filemtime(get_template_directory().'/style.css') ?></h2>
 </div>
 
 <?php 
